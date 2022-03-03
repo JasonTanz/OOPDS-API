5ed16ca85fed8b6c9e6921742912c26c2bac4463
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2022-03-03 12:55:58 +0800

    feat/initial-setup-done

--- src/main/java/oopds/assignment/DC/DcApplication.java
+++ src/main/java/oopds/assignment/DC/DcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DcApplication {
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(DcApplication.class, args);
 	}
 
