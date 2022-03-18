5ecd7a5b06553e6d227960c99ad894bfe1f2f37f
Vineshawn <100822201+Vineshawn@users.noreply.github.com>
2022-03-18 16:47:02 +0800

    updated controllers get by email & name

--- src/main/java/oopds/assignment/DC/DAOs/DonorDAO.java
+++ src/main/java/oopds/assignment/DC/DAOs/DonorDAO.java
@@ -1,6 +1,12 @@
 package oopds.assignment.DC.DAOs;
 
 import java.util.List;
-import oopds.assignment.DC.models.Ngo;
+import oopds.assignment.DC.models.Donor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
-public interface DonorDAO {}
+public interface DonorDAO extends JpaRepository<Donor, UUID> {
+
+    List<Donor> findByEmail(String email); // Abstract method
+    List<Donor> findByName(String name);
+}
--- src/main/java/oopds/assignment/DC/services/DonorService.java
+++ src/main/java/oopds/assignment/DC/services/DonorService.java
@@ -1,3 +1,35 @@
 package oopds.assignment.DC.services;
 
-public class DonorService {}
+import java.util.*;
+import oopds.assignment.DC.DAOs.DonorDAO;
+import oopds.assignment.DC.models.Donor;
+
+public class DonorService {
+
+    private DonorDAO donorDAO;
+
+	public List<Donor> getDonors() {
+		return donorDAO.findAll();
+	}
+
+	public Optional<Donor> getDonorsById(UUID id) {
+		return donorDAO.findById(id);
+	}
+
+	public List<Donor> getDonorsByEmail(String email){
+		List<Donor> donors = donorDAO.findByEmail(email);
+		if (donors.isEmpty())
+			return null;
+		else
+			return donors;
+	}
+
+	public List<Donor> getDonorsByName(String name){
+		List<Donor> donors = donorDAO.findByName(name);
+		if (donors.isEmpty())
+			return null;
+		else
+			return donors;
+	}
+
+}
