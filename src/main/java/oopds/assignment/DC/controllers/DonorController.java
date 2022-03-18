b99c449f7a08eac6872f4eadca596524c20c1396
thamjoeming <1201100409@student.mmu.edu.my>
2022-03-18 18:05:46 +0800

    Created a new class to return to response entity

--- src/main/java/oopds/assignment/DC/controllers/DonorController.java
+++ src/main/java/oopds/assignment/DC/controllers/DonorController.java
@@ -2,8 +2,10 @@ package oopds.assignment.DC.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import oopds.assignment.DC.DAOs.DonorDAO;
+import oopds.assignment.DC.models.DataResponse;
 import oopds.assignment.DC.models.Donor;
 import oopds.assignment.DC.services.DonorService;
 import org.springframework.http.HttpStatus;
@@ -25,40 +27,57 @@ public class DonorController {
 	DonorDAO donorDAO;
 
 	@GetMapping("/donor")
-	public ResponseEntity<List<Donor>> getAllDonors() {
+	public ResponseEntity< DataResponse<List<Donor>> > getAllDonors() {
 		try {
-			return new ResponseEntity<>(donorService.getDonors(), HttpStatus.OK);
+			DataResponse<List<Donor>> dataResponse = new DataResponse<>(donorService.getDonors(), "Operation Completed");
+			return new ResponseEntity<>(dataResponse, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/donor/{id}")
-	public ResponseEntity<Donor> getDonorById(@PathVariable("id") UUID id) {
+	public ResponseEntity< DataResponse<Donor> > getDonorById(@PathVariable("id") UUID id) {
 		try {
-			return new ResponseEntity<>(donorService.getDonorsById(id).get(), HttpStatus.OK);
+			Optional<Donor> donor = donorService.getDonorsById(id);
+			if (donor.isPresent()){
+				DataResponse<Donor> dataResponse = new DataResponse<>(donor.get(), "Operation Completed");
+				return new ResponseEntity<>(dataResponse, HttpStatus.OK);
+			}
+			else{
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/donor/{email}")
-	public ResponseEntity<List<Donor>> getDonorsByEmail(@PathVariable("email") String email) {
+	public ResponseEntity< DataResponse<List<Donor>> > getDonorsByEmail(@PathVariable("email") String email) {
 		try {
-			return new ResponseEntity<>(donorService.getDonorsByEmail(email), HttpStatus.OK);
+			List<Donor> list = donorService.getDonorsByEmail(email);
+			if (list == null)
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+			DataResponse<List<Donor>> dataResponse = new DataResponse<>(list, "Operation Completed");
+			return new ResponseEntity<>(dataResponse, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/donor/{name}")
-	public ResponseEntity<List<Donor>> getDonorsByName(@PathVariable("name") String name) {
+	public ResponseEntity< DataResponse<List<Donor>> > getDonorsByName(@PathVariable("name") String name) {
 		try {
-			return new ResponseEntity<>(donorService.getDonorsByName(name), HttpStatus.OK);
+			List<Donor> list = donorService.getDonorsByName(name);
+			if (list == null)
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+			DataResponse<List<Donor>> dataResponse = new DataResponse<>(list, "Operation Completed");
+			return new ResponseEntity<>(dataResponse, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
-	
 }
