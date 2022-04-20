f6fc6ee70b948b7c6b0a079c7be2e22828cd7aa1
ChihZhen <limcz99@gmail.com>
2022-04-20 23:26:53 +0800

    added queue model

---
+++ src/main/java/oopds/assignment/DC/models/NgoQueue.java
@@ -0,0 +1,63 @@
+package oopds.assignment.DC.models;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+public class NgoQueue {
+    @Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "id", nullable = false)
+	private UUID id;
+
+    @OneToMany
+    private List<Ngo> ngoList;
+
+    private String type;
+
+    public NgoQueue() {
+
+    }
+
+    public NgoQueue(List<Ngo> ngoList, String type) {
+        this.ngoList = ngoList;
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public List<Ngo> getNgoList() {
+        return this.ngoList;
+    }
+
+    public void setNgoList(List<Ngo> ngoList) {
+        this.ngoList = ngoList;
+    }
+
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
